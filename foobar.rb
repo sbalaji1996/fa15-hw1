d3db1cd857c202b44f0cd6a8ad8a795af83d1c2a
sbalaji1996 <srinand@berkeley.edu>
2016-02-12 17:41:18 -0800

    hw 1 solution

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,8 @@
 class Foobar
   # Q4 CODE HERE
+  def self.baz(a)
+    a.map { |i| i.to_i + 2 }.delete_if { |i| i % 2 != 0 || i > 10 }.uniq.inject(0, :+)
+    # inject(0, :+) found here: http://stackoverflow.com/questions/1538789/how-to-sum-array-of-numbers-in-ruby
+    # delete_if and uniq found here: http://www.tutorialspoint.com/ruby/ruby_arrays.htm
+  end
 end
